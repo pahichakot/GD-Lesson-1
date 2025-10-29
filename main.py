73c18639d2151764b07ffdcf05f2dc468dc8f5c1
pahichakot <swarnalatha.palteru@gmail.com>
2025-10-28 21:54:04 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,36 @@
+student = {
+    'name': 'Pahi',
+    'age': 11,
+    'hobby': "Dance",
+}
+
+print(student)
+print(student.keys())
+print(student.values())
+
+#Accessing values with keys
+print(student["age"])
+print(student["hobby"])
+
+for i in student.keys():
+    print(i," : ",student[i])
+
+if "marks" in student:
+    print("Key exists")
+else:
+    print("Key does not exist")
+
+student["favorite food"] = "dosa"
+print(student)
+
+del(student["hobby"])
+print(student)
+
+food = input("What is your favorite food : ")
+student["favorite food"] = food
+print(student)
+
+student["marks"] = [98, 76, 90, 94, 100]
+print(student["marks"])
+
+print(student["marks"][4])
